6d1c53ca428a36be7f8222d9081a390f87237b23
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-03-07 17:35:52 +0530

    Version2 Registration Component with validation working

--- src/components/RegisterCustomer.js
+++ src/components/RegisterCustomer.js
@@ -1,16 +1,84 @@
+import { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 
 function RegisterCust() {
+  const initialValues = {
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+    conpassword: "",
+  };
+
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const [formValues, setFormValues] = useState(initialValues);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit)
+      console.log(formErrors);
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "UserName is required";
+    }
+    if (!values.email) {
+      errors.email = "email is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a Valid Email format";
+    }
+    if (!values.phone) {
+      errors.phone = "phone is required";
+    }
+    if (!values.password) {
+      errors.password = "password is required ";
+    } else if (values.password.length < 4) {
+      errors.password = "password must be more than 4 character ";
+    } else if (values.password.length > 10) {
+      errors.password = "password can not ne more than 10  characters ";
+    }
+    if (!values.conpassword) {
+      errors.conpassword = "conpassword is required ";
+    } else if (values.conpassword !== values.password) {
+      errors.conpassword = "password is not matched ";
+    }
+
+    return errors;
+  };
   return (
-    <div className="bg-black">
-      <h1 className="text-light">This is Registration Page</h1>
-      <form action="">
-        <div className="row justify-content-center ">
+    <div className="container bg-black">
+      {/* <h1 className="text-light">This is Registration Page</h1> */}
+      <form onSubmit={handleSubmit}>
+        <div className="row justify-content-center m-0 ">
           <div
             className="col-6 d-flex align-items-center flex-column justify-content-center"
-            style={{ height: "500px", backgroundColor: "#48C9B0" }}
+            style={{ height: "700px", backgroundColor: "#48C9B0" }}
           >
-            <h2 className="text-center">Registration Form</h2>
+            <h3 className="text-center">Registration Form</h3>
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
+              <h3 className="ui message success text-light">
+                Registered Successfuly
+              </h3>
+            ) : (
+              <pre className="text-light">
+                {/* {JSON.stringify(formValues, undefined, 2)} */}
+              </pre>
+            )}
             <div>
               <div className="fs-5 text-dark fw-bold">User Name</div>
               <input
@@ -18,9 +86,13 @@ function RegisterCust() {
                 style={{ width: "300px" }}
                 type="text"
                 name="username"
-                id="username"
+                placeholder="username"
+                value={formValues.username}
+                onChange={handleChange}
               />
+              <p>{formErrors.username}</p>
             </div>
+
             <div>
               <div className="fs-5 text-dark fw-bold">Email Id</div>
               <input
@@ -28,18 +100,25 @@ function RegisterCust() {
                 style={{ width: "300px" }}
                 type="email"
                 name="email"
-                id="email"
+                placeholder="email"
+                value={formValues.email}
+                onChange={handleChange}
               />
+              <p>{formErrors.email}</p>
             </div>
+
             <div>
-              <div className="fs-5 text-dark fw-bold">phone number </div>
+              <div className="fs-5 text-dark fw-bold">Phone Number </div>
               <input
                 className="form-control"
                 style={{ width: "300px" }}
                 type="text"
-                name="phone "
-                id="phone"
+                name="phone"
+                placeholder="Phone Number"
+                value={formValues.phone}
+                onChange={handleChange}
               />
+              <p>{formErrors.phone}</p>
             </div>
 
             <div>
@@ -49,9 +128,13 @@ function RegisterCust() {
                 style={{ width: "300px" }}
                 type="password"
                 name="password"
-                id="password"
+                placeholder="Password"
+                value={formValues.password}
+                onChange={handleChange}
               />
+              <p>{formErrors.password}</p>
             </div>
+
             <div>
               <div className="fs-5 text-dark fw-bold">Confirm Password</div>
               <input
@@ -59,16 +142,20 @@ function RegisterCust() {
                 style={{ width: "300px" }}
                 type="password"
                 name="conpassword"
-                id="conpassword"
+                placeholder="Confirm Password"
+                value={formValues.conpassword}
+                onChange={handleChange}
               />
+              <p>{formErrors.conpassword}</p>
             </div>
+
             <div>
-              <input
+              <button
                 className="btn btn-primary m-2"
                 style={{ width: "200px" }}
-                type="button"
-                value="Register"
-              />
+              >
+                Register
+              </button>
             </div>
           </div>
         </div>
